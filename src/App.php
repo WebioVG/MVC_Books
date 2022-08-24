57b4e5ef1160e810ab51ce52cfd6b17d34c67884
webiovg <kevin.kowalsky1@gmaiL.com>
2022-08-24 11:35:58 +0200

    removed old comments

--- src/App.php
+++ src/App.php
@@ -6,15 +6,13 @@ class App extends \AltoRouter
 {
     public function run()
     {
-        // Renvoie la route actuelle
-        $match = $this->match();
+        $match = $this->match(); // current route
 
         if (is_array($match)) {
             [$controller, $method] = explode('@', $match['target']);
             $controller = 'M2i\\Mvc\\Controller\\'.$controller;
-            $call = new $controller(); // $call = new M2i\Mvc\Controller\UserController()
-            // fn(...[1, 2, 3]) devient fn(1, 2, 3)
-            $call->$method(...$match['params']); // $call->list();
+            $call = new $controller();
+            $call->$method(...$match['params']);
         } else {
             View::notFound();
         }
