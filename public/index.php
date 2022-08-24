0faaf545be8a82a7c507d0f29e305541270f9203
webiovg <kevin.kowalsky1@gmaiL.com>
2022-08-24 13:23:59 +0200

    added route to add a book

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ $app->addRoutes([
     ['GET', '/', 'HomeController@index'],
     ['GET', '/list', 'BookController@list'],
     ['GET', '/show/[:id]', 'BookController@show'],
+    ['GET', '/add', 'BookController@add'],
 ]);
 
 $app->run();
