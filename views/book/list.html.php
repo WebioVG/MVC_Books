9461f18a2e8b785acddfde7886f443d0639d4573
webiovg <kevin.kowalsky1@gmaiL.com>
2022-08-24 12:07:39 +0200

    taxed price is now shown

--- views/book/list.html.php
+++ views/book/list.html.php
@@ -6,7 +6,7 @@
                 <section class="bg-slate-400 rounded p-3">
                     <h3 class="font-bold"><?= $book->title ?></h3>
                     <hr class="block my-3">
-                    <p><span class="font-semibold">Price:</span> <?= $book->price ?>€</p>
+                    <p><span class="font-semibold">Price:</span> <?= $book->getPriceWithTaxes(20) ?>€</p>
                     <p><span class="font-semibold">ISBN:</span> <?= $book->isbn ?></p>
                     <p><span class="font-semibold">Author:</span> <?= $book->author ?></p>
                     <p><span class="font-semibold">Released:</span> <?= $book->releasedAtYear ?></p>
