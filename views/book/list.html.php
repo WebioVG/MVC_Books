ae8fcec0719d1616bdd5fa4604c215a0b52e7379
webiovg <kevin.kowalsky1@gmaiL.com>
2022-08-24 11:56:48 +0200

    all books are now listed

--- views/book/list.html.php
+++ views/book/list.html.php
@@ -1,7 +1,18 @@
 <?php require __DIR__.'/../partials/header.html.php'; ?>
     
-    <div class="max-w-5xl mx-auto">
-        <h2 class="text-center text-2xl">List</h2>
-    </div>
+    <main class="grid grid-cols-4 gap-10 p-4">
+        <?php if (! empty($books)) {
+            foreach ($books as $book) { ?>
+                <section class="bg-slate-400 rounded p-3">
+                    <h3 class="font-bold"><?= $book->title ?></h3>
+                    <hr class="block my-3">
+                    <p><span class="font-semibold">Price:</span> <?= $book->price ?>€</p>
+                    <p><span class="font-semibold">ISBN:</span> <?= $book->isbn ?></p>
+                    <p><span class="font-semibold">Author:</span> <?= $book->author ?></p>
+                    <p><span class="font-semibold">Released:</span> <?= $book->releasedAtYear ?></p>
+                </section>
+            <?php }
+        } ?>
+    </main>
 
 <?php require __DIR__.'/../partials/footer.html.php'; ?>
