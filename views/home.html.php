3d573cda711b48ed3bd370697798d959cd2971b2
webiovg <kevin.kowalsky1@gmaiL.com>
2022-08-24 11:39:59 +0200

    changed title tag

--- views/home.html.php
+++ views/home.html.php
@@ -1,7 +1,7 @@
 <?php require __DIR__.'/partials/header.html.php'; ?>
 
     <div claxéss="max-w-5xl mx-auto">
-        <h1 class="text-center text-2xl">HOME</h1>
+        <h2 class="text-center text-2xl">HOME</h2>
     </div>
 
 <?php require __DIR__.'/partials/footer.html.php'; ?>
