7d420304e4a770a7a2e700e7f4ac42224d3f50da
webiovg <kevin.kowalsky1@gmail.com>
2022-08-24 10:35:41 +0200

    footer added

--- views/partials/footer.html.php
+++ views/partials/footer.html.php
@@ -1,3 +1,5 @@
-		
+		<footer class="flex items-center justify-center h-[10vh] px-6 bg-slate-200 mt-4">
+			<p class="text-center w-[80%]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo ad ipsa ipsam quo, doloribus ullam. Dignissimos, officiis qui in delectus at, harum voluptate maxime saepe incidunt vel repudiandae facilis a!</p>
+		</footer>
 	</body>
 </html>
