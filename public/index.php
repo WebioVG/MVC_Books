c33e003b568d1f652e78fe1a18469599fc6947ec
webiovg <kevin.kowalsky1@gmail.com>
2022-08-24 09:17:48 +0200

    /\ initial commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use M2i\Mvc\App;
+
+require __DIR__.'/../vendor/autoload.php';
+
+define('BASE_URL', '/php-poo/perso/MVC_Books/public');
+
+$app = new App();
+$app->setBasePath(BASE_URL);
+
+$app->addRoutes([
+    ['GET', '/', 'HomeController@index'],
+]);
+
+$app->run();
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace M2i\Mvc\Controller;
+
+use M2i\Mvc\Controller\Controller;
+use M2i\Mvc\View;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return View::render('home');
+    }
+}
