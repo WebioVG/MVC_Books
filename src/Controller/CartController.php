29f4833e5d47f57fd8ad8888681b49dceec7daaa
Kévin Kowalsky <kevin.kowalsky1@gmail.com>
2022-08-25 14:57:07 +0200

    /\ creation of CartController - add method done

---
+++ src/Controller/CartController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace M2i\Mvc\Controller;
+
+use M2i\Mvc\Cart;
+use M2i\Mvc\Controller\Controller;
+use M2i\Mvc\Model\Book;
+use M2i\Mvc\View;
+
+class CartController extends Controller
+{
+    public function add($id)
+    {
+        $cart = new Cart();        
+        $cart->add((int) $id, 1);
+        $books = Book::all();
+
+        return View::render('book/list', [
+            'books' => $books
+        ]);
+    }
+}
